12c8fabf67efe6c1dce3893a86367f5fad842a0d
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-02 13:05:44 +0200

    added bodyParserMiddleware and jsonParserMiddleware

--- npm-express-middleware/server.js
+++ npm-express-middleware/server.js
@@ -4,17 +4,15 @@ import express from 'express';
 const staticFilesPath = path.join(process.cwd(), 'public');
 const serveStaticFilesMiddleware = express.static(staticFilesPath);
 const bodyParserMiddleware = express.urlencoded({ extended: true });
+const jsonParserMiddleware = express.json();
 
 const app = express();
 
-app.use(bodyParserMiddleware);
-
-app.use(serveStaticFilesMiddleware);
+app.use(bodyParserMiddleware, jsonParserMiddleware, serveStaticFilesMiddleware);
 
 app.post('/form', (req, res) => {
 	console.log(req.body);
-
-	res.send('Data received successfully!');
+	res.json(req.body);
 });
 
 app.listen(3000, () => {
